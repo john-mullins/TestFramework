20531b3144a79080ba19337cc17633d0767fdb48
John Mullins <john.mullins@thinci.com>
2018-08-27 17:03:45 +0100

    Tests for ASSERT_FALSE

--- tests/assertiontests.cpp
+++ tests/assertiontests.cpp
@@ -116,6 +116,7 @@ TEST(not_equals1)
         }
     }
 }
+
 TEST(not_equals1_msg)
 {
     try
@@ -130,6 +131,102 @@ TEST(not_equals1_msg)
         if (msg.find(expected) == std::string::npos)
         {
             throw UnitTests::TestFailure("Unexpected message from failing ASSERT_EQUALS", __FILE__, __LINE__);
+        }
+    }
+}
+
+TEST(assert_true)
+{
+    try
+    {
+        ASSERT_TRUE(1 == 1);
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        FAIL("ASSERT_TRUE should not have fired here.");
+    }
+}
+
+TEST(assert_true1)
+{
+    try
+    {
+        ASSERT_TRUE(1 == 2);
+        FAIL("ASSERT_TRUE should have fired here.");
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        std::string msg      = e.what();
+        std::string expected = "error A1000: Assertion failure :  Expression evaluated to false";
+        if (msg.find(expected) == std::string::npos)
+        {
+            throw UnitTests::TestFailure("Unexpected message from ASSERT_TRUE", __FILE__, __LINE__);
+        }
+    }
+}
+
+TEST(assert_true_msg)
+{
+    try
+    {
+        ASSERT_TRUE("Extra message", 1 == 2);
+        FAIL();
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        std::string msg      = e.what();
+        std::string expected = "error A1000: Assertion failure : Extra message Expression evaluated to false";
+        if (msg.find(expected) == std::string::npos)
+        {
+            throw UnitTests::TestFailure("Unexpected message from failing ASSERT_TRUE", __FILE__, __LINE__);
+        }
+    }
+}
+
+TEST(assert_false)
+{
+    try
+    {
+        ASSERT_FALSE(1 == 2);
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        FAIL("ASSERT_FALSE should not have fired here.");
+    }
+}
+
+TEST(assert_false1)
+{
+    try
+    {
+        ASSERT_FALSE(1 == 1);
+        FAIL("ASSERT_FALSE should have fired here.");
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        std::string msg      = e.what();
+        std::string expected = "error A1000: Assertion failure :  Expression evaluated to true";
+        if (msg.find(expected) == std::string::npos)
+        {
+            throw UnitTests::TestFailure("Unexpected message from ASSERT_TRUE", __FILE__, __LINE__);
+        }
+    }
+}
+
+TEST(assert_false_msg)
+{
+    try
+    {
+        ASSERT_FALSE("More Info", 1 == 1);
+        FAIL("ASSERT_FALSE should have fired here.");
+    }
+    catch (UnitTests::TestFailure& e)
+    {
+        std::string msg      = e.what();
+        std::string expected = "error A1000: Assertion failure : More Info Expression evaluated to true";
+        if (msg.find(expected) == std::string::npos)
+        {
+            throw UnitTests::TestFailure("Unexpected message from ASSERT_TRUE", __FILE__, __LINE__);
         }
     }
 }
