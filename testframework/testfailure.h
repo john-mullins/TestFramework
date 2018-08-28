5f7e5f8a9e4a0b4e82702e4b12abe5a0f3c813be
John Mullins <john.mullins@thinci.com>
2018-08-28 08:36:24 +0100

    Refactor common code

--- testframework/testfailure.h
+++ testframework/testfailure.h
@@ -19,8 +19,14 @@ namespace UnitTests
     class TestFailure : public std::exception
     {
     public:
+        TestFailure(
+            const std::string& msg, const std::string& file, int line, const std::string& failure_type, int error_code)
+            : m_what(FormatError(file, line, error_code) + failure_type + msg), m_msg(msg)
+        {
+        }
+
         TestFailure(const std::string& msg, const std::string& file, int line)
-            : m_what(FormatError(file, line, AssertionFailure) + "Assertion failure : " + msg), m_msg(msg)
+            : TestFailure(msg, file, line, "Assertion failure : ", AssertionFailure)
         {
         }
 
@@ -40,30 +46,13 @@ namespace UnitTests
         std::string m_msg;
     };
 
-    class TestTimeout : public std::exception
+    class TestTimeout : public TestFailure
     {
     public:
         TestTimeout(const std::string& msg, const std::string& file, int line)
-            : m_what(FormatError(file, line, TimeoutFailure) + "Test timeout : " + msg), m_msg(msg)
+            : TestFailure(msg, file, line, "Test timeout : ", TimeoutFailure)
         {
         }
-
-        const char* what() const noexcept override
-        {
-            return m_what.c_str();
-        }
-
-        // get the failure message WITHOUT the file and line number bit
-        std::string msg() const
-        {
-            return m_msg;
-        }
-
-        ~TestTimeout() noexcept override = default;
-
-    private:
-        std::string m_what;
-        std::string m_msg;
     };
 
     class TestSkipped : public std::exception
